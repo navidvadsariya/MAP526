489eb3c1171dfed8badeb5faa0b062c869c3e13a
Navid Vadsariya <nnvadsariya@myseneca.ca>
2021-02-09 20:09:51 -0500

    done

--- CashRegisterApplication/CashRegisterApplication/RestockPage.xaml.cs
+++ CashRegisterApplication/CashRegisterApplication/RestockPage.xaml.cs
@@ -13,7 +13,9 @@ namespace CashRegisterApplication
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RestockPage : ContentPage
     {
+        Product product;
         ObservableCollection<Product> localProductList;
+        int quantity;
         public RestockPage(ObservableCollection<Product> productList)
         {
             InitializeComponent();
@@ -22,18 +24,44 @@ namespace CashRegisterApplication
         }
 
         private void ProductRestockSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-
+        { 
+            product = (Product)e.SelectedItem;
         }
 
         private void RestockClicked(object sender, EventArgs e)
         {
+            if(product != null)
+            {
+                Boolean done = false;
 
+                quantity = int.Parse(updatedQuantity.Text);
+
+                if (quantity >= 0)
+                {
+                    for (int i = 0; i < localProductList.Count() && !done; i++)
+                    {
+                        if (String.Equals(localProductList[i].name, product.name))
+                        {
+                            localProductList[i].quantity += quantity;
+                            done = true;
+                        }
+                    }
+                }
+                else
+                {
+                    DisplayAlert("Alert", "Please enter valid quantity.", "OK");
+                }
+            }
+            else
+            {
+                DisplayAlert("Alert", "Please a product first.", "OK");
+            }
+            
         }
 
         private void CancelClicked(object sender, EventArgs e)
         {
-
+            Navigation.PopAsync();
         }
     }
 }
